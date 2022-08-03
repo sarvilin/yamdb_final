3eaeb4170041bc0dd455ff2e817ee286cc4278e6
Алексей Сарвилин <a.sarvilin@yandex.ru>
2022-08-03 15:00:58 +0300

    2.15

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -5,7 +5,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='foo')
 
-DEBUG = os.getenv('DEBUG', default=0)
+# DEBUG = os.getenv('DEBUG', default=0)
+DEBUG = True
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=['*'])
 ALLOWED_HOSTS = [
